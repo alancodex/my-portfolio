6b38f86a76c4c9663ec38bce03616b6b9377e971
PopCPS <arturcoelho.cps@gmail.com>
2024-10-04 17:35:01 -0300

    update 1.2

--- script.js
+++ script.js
@@ -34,3 +34,13 @@ function deleteText() {
 }
 
 type();
+
+const spotlight = document.querySelector('.spotlight');
+
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    spotlight.style.left = `${x - spotlight.offsetWidth / 2}px`;
+    spotlight.style.top = `${y - spotlight.offsetHeight / 2}px`;
+});
